f593f5c985f585a7d15f04ab7026c60bf7bd4f7b
Bertrand Dunogier <bertrand.dunogier@ez.no>
2019-10-04 18:19:24 +0200

    Finished query schema

--- src/lib/GraphQL/Schema/MatrixFieldDefinitionMapper.php
+++ src/lib/GraphQL/Schema/MatrixFieldDefinitionMapper.php
@@ -6,12 +6,30 @@
 
 namespace EzSystems\EzPlatformMatrixFieldtype\GraphQL\Schema;
 
+use eZ\Publish\API\Repository\ContentTypeService;
+use eZ\Publish\API\Repository\Values\ContentType\ContentType;
 use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
 use EzSystems\EzPlatformGraphQL\Schema\Domain\Content\Mapper\FieldDefinition\DecoratingFieldDefinitionMapper;
 use EzSystems\EzPlatformGraphQL\Schema\Domain\Content\Mapper\FieldDefinition\FieldDefinitionMapper;
 
 class MatrixFieldDefinitionMapper extends DecoratingFieldDefinitionMapper implements FieldDefinitionMapper
 {
+    /**
+     * @var \EzSystems\EzPlatformMatrixFieldtype\GraphQL\Schema\NameHelper
+     */
+    private $nameHelper;
+    /**
+     * @var \eZ\Publish\API\Repository\ContentTypeService
+     */
+    private $contentTypeService;
+
+    public function __construct(FieldDefinitionMapper $innerMapper, NameHelper $nameHelper, ContentTypeService $contentTypeService)
+    {
+        parent::__construct($innerMapper);
+        $this->nameHelper = $nameHelper;
+        $this->contentTypeService = $contentTypeService;
+    }
+
     protected function getFieldTypeIdentifier(): string
     {
         return 'ezmatrix';
@@ -23,7 +41,8 @@ class MatrixFieldDefinitionMapper extends DecoratingFieldDefinitionMapper implem
             return parent::mapToFieldValueType($fieldDefinition);
         }
 
-        return '[MatrixTypeMatrixFieldRow]';
+        $contentType = $this->findContentTypeOf($fieldDefinition);
+        return sprintf('[%s]', $this->nameHelper->matrixFieldDefinitionType($contentType, $fieldDefinition));
     }
 
     public function mapToFieldValueResolver(FieldDefinition $fieldDefinition): ?string
@@ -38,5 +57,20 @@ class MatrixFieldDefinitionMapper extends DecoratingFieldDefinitionMapper implem
         );
     }
 
+    private function findContentTypeOf(FieldDefinition $fieldDefinition): ContentType
+    {
+        foreach ($this->contentTypeService->loadContentTypeGroups() as $group) {
+            foreach ($this->contentTypeService->loadContentTypes($group) as $type) {
+                $foundFieldDefinition = $type->getFieldDefinition($fieldDefinition->identifier);
+                if ($foundFieldDefinition === null) {
+                    continue;
+                }
+                if ($foundFieldDefinition->id === $fieldDefinition->id) {
+                    return $type;
+                }
+            }
+        }
 
+        throw new \Exception("Could not find content type for field definition");
+    }
 }
